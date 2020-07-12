e2b7ad33994ed19ef0cc100b2cbd0d5202decd17
Tristan Oshier <Tristan.oshier2000@gmail.com>
2020-07-11 20:11:48 -0400

    add menu repository and create POST, GET, and, DELETE methods

--- CafeRepo/MenuRepository.cs
+++ CafeRepo/MenuRepository.cs
@@ -8,5 +8,28 @@ namespace CafeRepo
 {
     public class MenuRepository
     {
+        protected readonly List<Menu> _menuDirectory = new List<Menu>();
+
+        //POST
+        public bool AddMenuItem(Menu item)
+        {
+            int count = _menuDirectory.Count;
+            _menuDirectory.Add(item);
+            bool success = (_menuDirectory.Count > count) ? true : false;
+            return success;
+        }
+
+        //GET ALL
+        public List<Menu> GetAllMenuItems()
+        {
+            return _menuDirectory;
+        }
+
+        //DELETE
+        public bool DeleteMenuItem(Menu item)
+        {
+            bool success = _menuDirectory.Remove(item);
+            return success;
+        }
     }
 }
