a1885f2dc0eb919ba60223f11239563fe0f1f44c
Tristan Oshier <Tristan.oshier2000@gmail.com>
2020-07-12 00:30:31 -0400

    write get all test

---
+++ CompanyOutingsTests/OutingTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using CompanyOutingsRepository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompanyOutingsTests
+{
+    [TestClass]
+    public class OutingTest
+    {
+        [TestMethod]
+        public void GetAllOutings_ShouldReturnCorrectList()
+        {
+            Outing outing = new Outing();
+            OutingRepository outingRepo = new OutingRepository();
+            outingRepo.AddOuting(outing);
+
+            List<Outing> outings = outingRepo.GetAllOutings();
+            bool directoryContainsOuting = outings.Contains(outing);
+
+            Assert.IsTrue(directoryContainsOuting);
+        }
+    }
+}
