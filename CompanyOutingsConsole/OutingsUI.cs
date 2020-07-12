7684ed4bb89c724385cff007cd103a1ec9248d31
Tristan Oshier <Tristan.oshier2000@gmail.com>
2020-07-11 23:06:46 -0400

    create repository and add methods

---
+++ CompanyOutingsConsole/OutingsUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyOutingsConsole
+{
+    public class OutingsUI
+    {
+        //DISPLAY COST FOR ALL OUTINGS 
+
+        //DISPLAY COST FOR OUTINGS BY TYPE
+    }
+}
--- CompanyOutingsRepository/OutingRepository.cs
+++ CompanyOutingsRepository/OutingRepository.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace CompanyOutingsRepository
 {
-    class OutingRepository
+    public class OutingRepository
     {
+        protected readonly List<Outing> _outingDirectory = new List<Outing>();
+
+        //GET ALL
+        public List<Outing> GetAllOutings()
+        {
+            return _outingDirectory;
+        }
+
+        //POST
+        public bool AddOuting(Outing outing)
+        {
+            int count = _outingDirectory.Count;
+            _outingDirectory.Add(outing);
+            bool success = (_outingDirectory.Count > count) ? true : false;
+            return success;
+        }
+
+        //GET OUTINGS BY TYPE
+        public List<Outing> GetAllOutingsByType(string outingType)
+        {
+            List<Outing> specificTypeList = new List<Outing>();
+            foreach(Outing outing in _outingDirectory)
+            {
+                if(outing.TypeOfEvent.ToString().ToLower() == outingType.ToLower())
+                {
+                    specificTypeList.Add(outing);
+                }
+            }
+            return specificTypeList;
+        }
     }
 }
