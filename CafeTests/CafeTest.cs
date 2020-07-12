bccd9b0e729428ccb0e2bebc14a199375873457e
Tristan Oshier <Tristan.oshier2000@gmail.com>
2020-07-11 21:54:57 -0400

    write and run tests

--- CafeTests/CafeTest.cs
+++ CafeTests/CafeTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using CafeRepo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CafeTests
@@ -7,8 +9,39 @@ namespace CafeTests
     public class CafeTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddMenuItem_ShouldReturnCorrectBool()
         {
+            Menu item = new Menu();
+            MenuRepository menuRepo = new MenuRepository();
+
+            bool itemAdded = menuRepo.AddMenuItem(item);
+
+            Assert.IsTrue(itemAdded);
+        }
+
+        [TestMethod]
+        public void GetAllMenuItems_ShouldReturnCorrectList()
+        {
+            Menu item = new Menu();
+            MenuRepository menuRepo = new MenuRepository();
+            menuRepo.AddMenuItem(item);
+
+            List<Menu> menuItems = menuRepo.GetAllMenuItems();
+            bool directoryContainsItem = menuItems.Contains(item);
+
+            Assert.IsTrue(directoryContainsItem);
+        }
+
+        [TestMethod]
+        public void DeleteMenuItem_ShouldReturnTrue()
+        {
+            Menu item = new Menu(1, "Coffee", "Our house made coffee", "coffee beans, water", 3.99);
+            MenuRepository menuRepo = new MenuRepository();
+            menuRepo.AddMenuItem(item);
+
+            bool itemRemoved = menuRepo.DeleteMenuItem(item);
+
+            Assert.IsTrue(itemRemoved);
         }
     }
 }
