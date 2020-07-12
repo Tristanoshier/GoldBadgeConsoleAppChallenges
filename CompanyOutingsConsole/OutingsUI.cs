44ae90e052f7fa37d6b6fbe0c719640c9586b089
Tristan Oshier <Tristan.oshier2000@gmail.com>
2020-07-12 00:11:57 -0400

    add specific outing cost method to UI

--- CompanyOutingsConsole/OutingsUI.cs
+++ CompanyOutingsConsole/OutingsUI.cs
@@ -36,16 +36,16 @@ namespace CompanyOutingsConsole
                 switch(userInput)
                 {
                     case "1":
-                        //SeeAllOutings();
+                        SeeAllOutings();
                         break;
                     case "2":
-                        //AddOuting();
+                        AddOuting();
                         break;
                     case "3":
-                        //SeeCostOfAllOutings();
+                        SeeCostOfAllOutings();
                         break;
                     case "4":
-                        //SeeSpecificOutingTypeCost();
+                        SeeSpecificOutingTypeCost();
                         break;
                     case "5":
                         continueToRun = false;
@@ -120,14 +120,63 @@ namespace CompanyOutingsConsole
 
         private void SeeCostOfAllOutings()
         {
+            double total = 0;
+            List<Outing> outings = _outingRepo.GetAllOutings();
 
+            foreach(Outing outing in outings)
+            {
+                total += outing.TotalCostOfEvent;
+            }
+            Console.WriteLine($"The total cost for all outings is ${total}.\n" +
+                $"Press any key to continue...");
+            Console.ReadKey();
         }
 
         private void SeeSpecificOutingTypeCost()
         {
+            List<Outing> outings = _outingRepo.GetAllOutings();
+            Console.WriteLine
+                (
+                    "Please select an outing type\n" +
+                    "1.) Golf\n" +
+                    "2.) Bowling\n" +
+                    "3.) Amusement Park\n" +
+                    "4.) Concert"
+                );
+            string typeInput = Console.ReadLine();
+            string userChoice;
+            switch (typeInput)
+            {
+                case "1":
+                    userChoice = "golf";
+                    break;
+                case "2":
+                    userChoice = "bowling";
+                    break;
+                case "3":
+                    userChoice = "amusementpark";
+                    break;
+                case "4":
+                    userChoice = "concert";
+                    break;
+                default:
+                    userChoice = null;
+                    break;
+            }
 
+            double total = 0;
+            foreach (Outing outing in outings)
+            {
+                if(outing.TypeOfEvent.ToString().ToLower() == userChoice)
+                {
+                    total += outing.TotalCostOfEvent;
+                }
+            }
+            Console.WriteLine($"All {userChoice} outings for the year were ${total}\n" +
+                $"Press any key to continue...");
+            Console.ReadKey();
         }
 
-
+        
     }
 }
