12a7feabd47419e41ae641308338f0f3235aeafc
Tristan Oshier <Tristan.oshier2000@gmail.com>
2020-07-11 22:35:18 -0400

    create Outing class

---
+++ CompanyOutingsRepository/Outing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyOutingsRepository
+{
+    public enum EventType { Golf, Bowling, AmusementPark, Concert }
+    public class Outing
+    {
+        public Outing()
+        {
+        }
+
+        public Outing(int numOfPeopleAttended, DateTime date, double costPerPerson, EventType typeOfEvent)
+        {
+            NumOfPeopleAttended = numOfPeopleAttended;
+            Date = date;
+            CostPerPerson = costPerPerson;
+            TypeOfEvent = typeOfEvent;
+        }
+
+        public int NumOfPeopleAttended { get; set; }
+        public DateTime Date { get; set; }
+        public double CostPerPerson { get; set; }
+        public EventType TypeOfEvent { get; set; }
+        public double TotalCostOfEvent
+        {
+            get
+            {
+                return NumOfPeopleAttended * CostPerPerson;
+            }
+        }
+    }
+}
