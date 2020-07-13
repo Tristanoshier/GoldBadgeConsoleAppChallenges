ad287d98faff1be12be63fefa276188fe548336c
Tristan Oshier <Tristan.oshier2000@gmail.com>
2020-07-13 08:36:49 -0400

    add customer class and properties

---
+++ GreetingRepo/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingRepo
+{
+    public enum CustomerType { Potential, Current, Past }
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+
+        public Customer(string firstName, string lastName, CustomerType typeOfCustomer)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            TypeOfCustomer = typeOfCustomer;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public CustomerType TypeOfCustomer { get; set; }
+        public string Email
+        {
+            get
+            {
+                if(TypeOfCustomer == CustomerType.Potential)
+                {
+                    return "We currently have the lowest rates on Helicopter Insurance!";
+                }
+                else if (TypeOfCustomer == CustomerType.Current)
+                {
+                    return "Thank you for your work with us. We appreciate your loyalty. Here's a coupon.";
+                } 
+                else
+                {
+                    return "It's been a long time since we've heard from you, we want you back.";
+                }
+            }
+        }
+
+    }
+}
---
+++ GreetingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GreetingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GreetingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00891f0b-88b5-4555-a15d-9ccd4e64c191")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
