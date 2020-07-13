f388ea7aaef9840015b931978e0327217c8b2484
Tristan Oshier <Tristan.oshier2000@gmail.com>
2020-07-13 08:47:22 -0400

    create get, post, and delete methods

--- GreetingRepo/CustomerRepository.cs
+++ GreetingRepo/CustomerRepository.cs
@@ -8,5 +8,36 @@ namespace GreetingRepo
 {
     public class CustomerRepository
     {
+        protected readonly List<Customer> _customerDirectory = new List<Customer>();
+
+        //GET
+        public List<Customer> GetAllCustomers()
+        {
+            //return this in alphabetical order
+            return _customerDirectory;
+        }
+
+        //POST
+        public bool AddCustomer(Customer customer)
+        {
+            int count = _customerDirectory.Count;
+            _customerDirectory.Add(customer);
+            bool success = (_customerDirectory.Count > count) ? true : false;
+            return success;
+        }
+
+        //UPDATE
+        public bool UpdateCustomerInfo(Customer customer)
+        {
+
+        }
+
+        //DELETE
+        public bool DeleteMenuItem(Customer customer)
+        {
+            bool success = _customerDirectory.Remove(customer);
+            return success;
+        }
+       
     }
 }
